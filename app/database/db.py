3ea1479a5fa25e91f5d77d0b42ecc3dc339fcc23
Mario <mariorubiodev@gmail.com>
2023-09-01 13:17:17 +0200

    Cambios en psycop2-binary

--- app/database/db.py
+++ app/database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import create_engine
 
-engine = create_engine('postgresql://mrubio:secret@192.168.0.27:5432/hospederia')
+engine = create_engine('postgresql+psycopg2://mrubio:secret@localhost:5432/hospederia')
 Base = declarative_base()
 
 SessionLocal = sessionmaker(bind=engine)
